4a128fbdc7b63c5e2aab3f93902edc1d751304fd
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-03-04 17:34:28 -0600

    Implementación de tools service para consulta en base de datos

--- src/tools/tools.service.ts
+++ src/tools/tools.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateToolDto } from './dto/create-tool.dto';
+import { UpdateToolDto } from './dto/update-tool.dto';
+import { Tool } from './schemas/tool.schema';
 
 @Injectable()
-export class ToolsService {}
+export class ToolsService {
+    constructor(@InjectModel(Tool.name) private toolModel: Model<Tool>){}
+
+    async getAllTools(): Promise<Tool[]>{
+        return this.toolModel.find(); 
+    }
+
+    async createTool(tool: CreateToolDto){
+        const newTool = new this.toolModel(tool);
+        return newTool.save();
+    }
+
+    async deleteTool(id: number){
+        return this.toolModel.findOneAndDelete({id});
+    }
+
+    async findOneTool(id: number){
+        return this.toolModel.findOne({id});
+    }
+
+    async updateTool(id:number, tool: UpdateToolDto){
+        return this.toolModel.findOneAndUpdate( {id}, tool, {new: true});
+    }
+}
