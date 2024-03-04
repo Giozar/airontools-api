0a4f4403ee6a28882355f2f77263083045f5da00
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-03-04 17:13:58 -0600

    Conexión a base de datos MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ToolsModule } from './tools/tools.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ToolsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:2701/airontools'),
+    ToolsModule
+  ],
   controllers: [],
   providers: [],
 })
