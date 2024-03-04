f3e001ed8f830c9fb88e0cbeb6d320fb6b2840fb
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-03-04 17:21:56 -0600

    Implementación de tool schema

---
+++ src/tools/schemas/tools.schema.ts
@@ -0,0 +1,56 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+
+export type toolDocument = HydratedDocument<Tool>;
+
+@Schema()
+export class Tool {
+    @Prop({
+        // Lo comento para modificar las id's de los productos para nerlos en null y orderar los id´s
+        unique: true,
+        required: true,
+    })
+    id: number;
+    
+    @Prop({
+        unique: true,
+        required: true,
+    })
+    name: string;
+
+    @Prop({
+        required: true,
+    })
+    categoryId: number;
+    
+    @Prop({
+        required: true,
+    })
+    subcategoryId: number;
+    
+    @Prop()
+    subsubcategoryId: number;
+    
+    @Prop()
+    path: string;
+
+    @Prop()
+    image: string;
+
+    @Prop()
+    overview: string;
+
+    @Prop()
+    description: string;
+    
+    @Prop()
+    advantages: string;
+    
+    @Prop()
+    specification: string;
+    
+    
+}
+
+export const ToolSchema = SchemaFactory.createForClass(Tool);
--- src/tools/tools.module.ts
+++ src/tools/tools.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ToolsService } from './tools.service';
 import { ToolsController } from './tools.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tool, ToolSchema } from './schemas/tools.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    {
+      name: Tool.name,
+      schema: ToolSchema,
+    }
+  ])],
   providers: [ToolsService],
   controllers: [ToolsController]
 })
