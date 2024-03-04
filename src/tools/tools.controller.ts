ae96e7d4c9ab8f90fa9ed4a335cf545d31c7073a
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-03-04 17:37:53 -0600

    Implementación de tools controller para realizar peticiones CRUD

--- src/tools/tools.controller.ts
+++ src/tools/tools.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Body, ConflictException, Controller, Delete, Get, HttpCode, NotFoundException, Param, Post, Put, Query } from '@nestjs/common';
+import { ToolsService } from './tools.service';
+import { CreateToolDto } from './dto/create-tool.dto';
+import { UpdateToolDto } from './dto/update-tool.dto';
 
 @Controller('tools')
-export class ToolsController {}
+export class ToolsController {
+    
+    constructor(private toolsService : ToolsService){}
+    
+    @Get()
+    getAllTools(){
+        return this.toolsService.getAllTools();
+    }
+
+    @Get(':id')
+    async findOneTool(@Param('id') id: number) {
+        const tool = await this.toolsService.findOneTool(id);
+        if(!tool) throw new NotFoundException('Tool not found');
+        return tool;
+    }
+
+    @Post()
+    async createTool(@Body() body: CreateToolDto){
+        try {
+            return await this.toolsService.createTool(body);
+        } catch (error) {
+            if( error.code === 11000){
+                throw new ConflictException('Tool already exists');
+            }
+            throw error;
+        }
+    }
+
+    @Delete(':id')
+    @HttpCode(204)
+    async deleteTool(@Param('id') id: number){
+        const tool = await this.toolsService.deleteTool(id);
+        if(!tool) throw new NotFoundException('Tool not found');
+        return tool;
+    }
+
+    @Put(':id')
+    async updateTool(@Param('id') id: number, @Body() body:UpdateToolDto){
+        const tool = await this.toolsService.updateTool(id, body);
+        if(!tool) throw new NotFoundException('Tool not found');
+        return tool;
+    }
+}
