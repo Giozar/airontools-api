c924bf9cfbc9d022ce9d196ea3eda498957bb85b
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-03-05 16:16:34 -0600

    Implementación de petición get para obtener todas la categorias

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Category } from './schemas/category.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(@InjectModel(Category.name) private categoryModel: Model<Category>){}
+
+    async getAllCategories(): Promise<Category[]>{
+        return this.categoryModel.find();
+    }
+}
