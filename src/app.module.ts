56cc0cbdd201f4f224e51c92ba73f9b41092ef80
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-03-07 14:43:44 -0600

    Implementación de archivo de configuración y uso de variables de entorno en archivo .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
+import configuration from './config/configuration';
 import { Module } from '@nestjs/common';
 import { ToolsModule } from './tools/tools.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriesModule } from './categories/categories.module';
 import { SubcategoriesModule } from './subcategories/subcategories.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/airontools'),
+    ConfigModule.forRoot({
+      load: [ configuration ],
+    }),
+    MongooseModule.forRoot( configuration().database.host ),
     ToolsModule,
     CategoriesModule,
     SubcategoriesModule
