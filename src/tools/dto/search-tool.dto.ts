a1acb8d96556d6b2f9aed393f7deafaa997b0f56
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-04-15 16:49:17 -0600

    Validación de datos en SearchToolDto

--- src/tools/dto/search-tool.dto.ts
+++ src/tools/dto/search-tool.dto.ts
@@ -1,4 +1,12 @@
+import { IsAlphanumeric, IsOptional } from "class-validator";
+
 export class SearchToolDto {
+
+    @IsOptional()
+    @IsAlphanumeric()
     keyword1: string;
+
+    @IsOptional()
+    @IsAlphanumeric()
     keyword2: string;
 }
