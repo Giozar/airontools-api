133e2d99d37ceea280cfeeb9f92582a211c93f3f
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-06-12 01:53:40 -0600

    Implementación de funcionalidad para login de usuario en auth controller y service, creación de dto para login de usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import * as bcrypt from 'bcrypt';
@@ -8,12 +8,13 @@ import { handleDBErrors } from './handlers/auth.errors';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import { LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async create(createUserDto: CreateUserDto) {
+  async createUser(createUserDto: CreateUserDto) {
     try {
       const { password, ...UserData } = createUserDto;
       const newUser = new this.userModel({
@@ -31,5 +32,28 @@ export class AuthService {
     } catch (error) {
       handleDBErrors(error);
     }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+
+    // Find user by email and select the password and email
+    const user = await this.userModel
+      .findOne({ email })
+      .select('password email');
+
+    if (!user) {
+      throw new UnauthorizedException(
+        'Credentials are invalid (email is incorrect)',
+      );
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException(
+        'Credentials are invalid (password is incorrect)',
+      );
+    }
+
+    return user;
   }
 }
