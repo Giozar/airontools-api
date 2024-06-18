d176d3391ab4182972d0fbbc11105a22fee8521f
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-06-18 02:21:14 -0600

    refactor: Se actualiza main.ts para usar variable de entorno del número del puerto de la aplicación api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('bootstrap');
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -13,6 +14,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 3000);
+  logger.log(`Application listening on port ${process.env.PORT || 3000}`);
 }
 bootstrap();
