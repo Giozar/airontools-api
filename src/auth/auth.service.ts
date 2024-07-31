0bddd9740190a44d6f16c42d7951b3031b880a31
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-07-31 17:33:47 -0600

    feat: Se implementan populate para obtner el rol de usuario en auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,6 +15,7 @@ import { JwtService } from '@nestjs/jwt';
 import { handleDBErrors } from 'src/handlers/error.handle';
 @Injectable()
 export class AuthService {
+  private ROLE = 'role';
   constructor(
     @InjectModel(User.name)
     private userModel: Model<User>,
@@ -24,13 +25,17 @@ export class AuthService {
   async createUser(createUserDto: CreateUserDto) {
     try {
       const { password, ...UserData } = createUserDto;
-      const user = new this.userModel({
+      const createdUser = new this.userModel({
         ...UserData,
         password: await bcrypt.hashSync(password, 10),
       });
 
-      await user.save();
+      await createdUser.save();
 
+      const user = await this.userModel
+        .findById(createdUser._id)
+        .populate(this.ROLE)
+        .exec();
       // Exclude password from the response
       user.password = undefined;
 
@@ -45,7 +50,7 @@ export class AuthService {
   }
 
   async getUsers(): Promise<User[]> {
-    return await this.userModel.find().populate('role').exec();
+    return await this.userModel.find().populate(this.ROLE).exec();
   }
 
   async getUser(id: string): Promise<User> {
@@ -53,7 +58,10 @@ export class AuthService {
       if (!mongoose.Types.ObjectId.isValid(id)) {
         throw new BadRequestException('ID de usuario no válido');
       }
-      const userFound = await this.userModel.findById(id);
+      const userFound = await this.userModel
+        .findById(id)
+        .populate(this.ROLE)
+        .exec();
       if (!userFound) throw new NotFoundException('Usuario no encontrado');
       return userFound;
     } catch (error) {
@@ -74,9 +82,12 @@ export class AuthService {
         user.password = bcrypt.hashSync(password, 10);
       }
 
-      const userUpdated = await this.userModel.findByIdAndUpdate(id, user, {
-        new: true,
-      });
+      const userUpdated = await this.userModel
+        .findByIdAndUpdate(id, user, {
+          new: true,
+        })
+        .populate(this.ROLE)
+        .exec();
 
       if (!userUpdated) {
         throw new NotFoundException('Usuario no encontrado');
@@ -90,7 +101,8 @@ export class AuthService {
 
   async deleteUser(id: string) {
     const deletedUser = await this.userModel
-      .findByIdAndDelete({ _id: id })
+      .findByIdAndDelete(id)
+      .populate(this.ROLE)
       .exec();
     if (!deletedUser) throw new NotFoundException('Usuario no encontrado');
     return deletedUser;
@@ -102,7 +114,9 @@ export class AuthService {
     // Find user by email and select the password and email
     const user = await this.userModel
       .findOne({ email })
-      .select('password email _id roles fullName imageUrl');
+      .populate(this.ROLE)
+      .select('password email _id role fullName imageUrl')
+      .exec();
 
     if (!user) {
       throw new UnauthorizedException(
