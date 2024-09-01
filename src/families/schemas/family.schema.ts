17aae10b33f5741d6f231e614de41cee8fa70b8c
Giovanni Cortazar <manuelgiozar04@gmail.com>
2024-09-01 02:48:05 -0600

    chore: Se agrega comentario de middleware en family schema

--- src/families/schemas/family.schema.ts
+++ src/families/schemas/family.schema.ts
@@ -36,3 +36,33 @@ export class Family {
 }
 
 export const FamilySchema = SchemaFactory.createForClass(Family);
+
+// Middleware para eliminar categorías relacionadas
+/*FamilySchema.pre('findOne', async function (next) {
+  const family = this.getQuery()['_id'];
+
+  try {
+    // Aquí buscamos todas las categorías relacionadas con la familia
+    const Category = mongoose.model('Category', CategorySchema);
+
+    const x = Category;
+
+    x.find();
+
+    // console.log(x.findOne({ family }));
+
+    //const category = await Category.exists({ family });
+
+    // console.log(category);
+
+    console.log(`Buscamos todas las categorías con la familia ${family}`);
+    next();
+  } catch (error) {
+    console.error(
+      `Error al buscar categorías para la familia ${family}:`,
+      error,
+    );
+    next(error);
+  }
+});
+*/
